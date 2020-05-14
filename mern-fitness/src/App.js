b4faeeddb429d9150d5590211db2e122f660804a
jordan <jordan@jordans-Air.frontierlocal.net>
2020-05-14 14:45:50 -0700

    began frontend

--- mern-fitness/src/App.js
+++ mern-fitness/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>sanity check</h1>
     </div>
+    </Router>
   );
 }
 
