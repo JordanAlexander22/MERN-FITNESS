90e8e0220aec6a71c9ee09282df9fcc622be3bb0
jordan <jordan@jordans-Air.frontierlocal.net>
2020-05-16 14:37:20 -0700

    navbar basic

--- mern-fitness/src/components/Navbar/Navbar.jsx
+++ mern-fitness/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-export default class Navbar extends Component {
-    render() {
-        return (
-            <nav className= 'navbar navbar-dark bg-dark navbar-expand-lg'>
-                <Link to= "/" className= 'navbar-brand'>ExerTracker</Link>
-            </nav>
-        )
-    }
+export default function Navbar() {
+	return (
+		<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+			<Link to="/" className="navbar-brand">ExerTracker</Link>
+			<div className="collapse navbar-collapse">
+			<ul className="navbar-nav mr-auto">
+				<li className="navbar-item">
+				<Link to="/" className="nav-link">Exercises</Link>
+                </li>
+				<li className="navbar-item">
+				<Link to="/create" className="nav-link">Create exercise</Link>
+                </li>
+				<li className="navbar-item">
+				<Link to="/user" className="nav-link">Create User</Link>
+				</li>
+			</ul>
+			</div>
+		</nav>
+	);
 }
