d50c5cbc55690c99265c14fc2be128298dc48d60
jordan <jordan@jordans-Air.frontierlocal.net>
2020-05-27 19:23:40 -0700

    need to add jsx to editexercise

--- mern-fitness/src/components/EditExercise/EditExercise.jsx
+++ mern-fitness/src/components/EditExercise/EditExercise.jsx
@@ -1,28 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css'
+import 'react-datepicker/dist/react-datepicker.css';
 import axios from 'axios';
 
-
 export default class EditExercise extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            username: '',
-            description: '',
-            duration: 0,
-            date: new Date(),
-            users: []
-        }
-    }
+		this.state = {
+			username: '',
+			description: '',
+			duration: 0,
+			date: new Date(),
+			users: []
+		};
+	}
 
-    handleUserNameChange = (e) => {
+	handleUserNameChange = (e) => {
 		let username = e.target.value;
 		this.setState({ username });
-    };
-    
-    handleDescriptionChange = (e) => {
+	};
+
+	handleDescriptionChange = (e) => {
 		let description = e.target.value;
 		this.setState({ description });
 	};
@@ -36,29 +35,47 @@ export default class EditExercise extends Component {
 		this.setState({
 			date: date
 		});
-    };
+	};
 
-    componentDidMount() {
-        axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
-        .then (response => {
-            this.setState({
-                username: response.data.username,
-                description: response.data.description,
-                duration: response.data.duration,
-                date: new Date(response.data.date)
-            })
+	componentDidMount() {
+		axios.get('http://localhost:5000/exercises/' + this.props.match.params.id)
+			.then((response) => {
+				this.setState({
+					username: response.data.username,
+					description: response.data.description,
+					duration: response.data.duration,
+					date: new Date(response.data.date)
+				});
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
 
-        })
-        .catch(function (error) {
-            console.log(error)
+        axios.get('hhtp://localhost:5000/users/')
+        .then(response => {
+            this.setState({users: response.data.map(user => user.username) });
         })
     }
+    
+    onSubmit = (e) => {
+        e.preventDefault();
 
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date,
+        };
 
+        console.log(exercise);
 
-    render() {
-        return (
-           <h1>EditExercise Component</h1>
-        )
+        axios.post('http://localhost:5000/exercises/update/'+this.props.match.params.id, exercise)
+        .then (res => console.log(res.data))
+
+        //window.location = '/'
     }
+
+	render() {
+		return <h1>EditExercise Component</h1>;
+	}
 }
