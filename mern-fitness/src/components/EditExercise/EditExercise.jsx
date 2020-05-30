61514d964d193856fe99f2f829609c3ececbf23e
jordan <jordan@jordans-Air.frontierlocal.net>
2020-05-30 14:41:02 -0700

    edit exercise roughly working

--- mern-fitness/src/components/EditExercise/EditExercise.jsx
+++ mern-fitness/src/components/EditExercise/EditExercise.jsx
@@ -38,7 +38,8 @@ export default class EditExercise extends Component {
 	};
 
 	componentDidMount() {
-		axios.get('http://localhost:5000/exercises/' + this.props.match.params.id)
+		axios
+			.get('http://localhost:5000/exercises/' + this.props.match.params.id)
 			.then((response) => {
 				this.setState({
 					username: response.data.username,
@@ -51,31 +52,81 @@ export default class EditExercise extends Component {
 				console.log(error);
 			});
 
-        axios.get('hhtp://localhost:5000/users/')
-        .then(response => {
-            this.setState({users: response.data.map(user => user.username) });
-        })
-    }
-    
-    onSubmit = (e) => {
-        e.preventDefault();
+		axios.get('hhtp://localhost:5000/users/').then((response) => {
+			this.setState({ users: response.data.map((user) => user.username) });
+		});
+	}
 
-        const exercise = {
-            username: this.state.username,
-            description: this.state.description,
-            duration: this.state.duration,
-            date: this.state.date,
-        };
+	onSubmit = (e) => {
+		e.preventDefault();
 
-        console.log(exercise);
+		const exercise = {
+			username: this.state.username,
+			description: this.state.description,
+			duration: this.state.duration,
+			date: this.state.date
+		};
 
-        axios.post('http://localhost:5000/exercises/update/'+this.props.match.params.id, exercise)
-        .then (res => console.log(res.data))
+		console.log(exercise);
 
-        //window.location = '/'
-    }
+		axios
+			.post('http://localhost:5000/exercises/update/' + this.props.match.params.id, exercise)
+			.then((res) => console.log(res.data));
+
+		//window.location = '/'
+	};
 
 	render() {
-		return <h1>EditExercise Component</h1>;
+		return (
+			<div>
+				<h3>Edit Exercise Log</h3>
+				<form onSubmit={this.onSubmit}>
+					<div className="form-group">
+						<label>Username: </label>
+						<select
+							ref="userInput"
+							className="form-control"
+							value={this.state.username}
+							onChange={this.handleUserNameChange}
+						>
+							{this.state.users.map(function(user) {
+								return (
+									<option key={user} value={user}>
+										{user}
+									</option>
+								);
+							})}
+						</select>
+					</div>
+					<div className="form-group">
+						<label>Description: </label>
+						<input
+							type="text"
+							required
+							className="form-control"
+							value={this.state.description}
+							onChange={this.handleDescriptionChange}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Duration (minutes): </label>
+						<input
+							type="text"
+							className="form-control"
+							value={this.state.duration}
+							onChange={this.handleDurationChange}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Date: </label>
+						<DatePicker selected={this.state.date} onChange={this.onChangeDate} />
+					</div>
+
+					<div className="form-group">
+						<input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
